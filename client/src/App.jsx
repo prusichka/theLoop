2b8accce341d48fbdea1fd31c9032714f87e3585
YuraMell <yura.melnik7523@gmail.com>
2022-01-14 17:03:58 +0200

    add auth routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,12 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
-import { Layout } from './components/partials/Layout';
-import { About } from './components/pages/About';
-import { Contacts } from './components/pages/Contacts';
-import { Home } from './components/pages/Home';
-import { NotFound } from './components/pages/NotFound';
-import SingleItem from "./components/pages/SingleItem";
-import { Products } from "./components/pages/Products";
-
+import { useRoutes } from './routes';
 import './style.scss'
 
 function App() {
+  const routes = useRoutes(false)
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='contacts' element={<Contacts />} />
-          <Route path='products' element={<Products />} />
-          <Route path='category' element={<Products />} >
-            <Route path='*' element={<Products />} />
-          </Route>
-          <Route path='type' element={<Products />} >
-            <Route path='*' element={<Products />} />
-          </Route>
-          <Route path='*' element={<NotFound />} />
-          <Route path='single-item' element={<SingleItem />} >
-            <Route path=':id' element={<SingleItem />} />
-          </Route>
-        </Route>
-      </Routes>
+      {routes}
     </>
   );
 }
